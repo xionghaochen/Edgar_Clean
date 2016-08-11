11a464f9fa9fb643583ed64137d598d1a3957524
Haochen Xiong <xhaochen@udel.edu>
2016-08-11 02:23:39 -0400

    New Version

--- edgar_advanced.py
+++ edgar_advanced.py
@@ -9,16 +9,18 @@ import os
 import shutil
 import sys
 import re
+from bs4 import BeautifulSoup
+
 
 def main():
 
     for dirpath,dirnames,filenames in os.walk(os.path.abspath('.')):
         for filename in filenames:
-            if os.path.splitext(filename)[1]=='.idx':
+            if os.path.splitext(filename)[1] == '.idx':
                 if os.path.splitext(filename)[0] in dirnames:
-                    original_dir=os.path.join(os.path.abspath('.'),os.path.splitext(filename)[0])
+                    original_dir = os.path.join(os.path.abspath('.'),os.path.splitext(filename)[0])
 
-                    cleaned_dir = os.path.join(os.path.abspath('.'), os.path.splitext(filename)[0]+'_CLEANED')
+                    cleaned_dir = os.path.join(os.path.abspath('.'), os.path.splitext(filename)[0] + '_CLEANED')
 
                     if os.path.isdir(cleaned_dir):
                         shutil.rmtree(cleaned_dir)
@@ -26,102 +28,96 @@ def main():
                     if not os.path.isdir(cleaned_dir):
                         os.mkdir(cleaned_dir)
 
-                    if not os.path.isdir(os.path.join(cleaned_dir, 'Manual_Clean')):
-                        os.mkdir(os.path.join(cleaned_dir, 'Manual_Clean'))
-
                 else:
-                    sys.exit('Error: Original directory %r cannot be found'%os.path.splitext(filename)[0])
+                    sys.exit('Error: Original directory %r cannot be found' % os.path.splitext(filename)[0])
 
     traverse_folders(original_dir,cleaned_dir)
 
+
 def traverse_folders(original_dir,cleaned_dir):
 
-    original_folders=os.listdir(original_dir)
+    original_folders = os.listdir(original_dir)
 
-    count=0
+    count = 0
 
     for f in original_folders:
 
-        if f=='ConnError.txt' or f=='IOError.txt' or f=='Not_Found.txt' or f=='.DS_Store'or os.path.splitext(f)[1].lower()=='.swp':
+        if f == 'ConnError.txt' or f == 'IOError.txt' or f == 'Not_Found.txt' or f == '.DS_Store'or os.path.splitext(f)[1].lower() == '.swp':
             continue
-        if os.path.splitext(f)[1].lower()=='.pdf' or os.path.splitext(f)[1].lower()=='.xml' or os.path.splitext(f)[1].lower()=='.paper':
+        if os.path.splitext(f)[1].lower() == '.pdf' or os.path.splitext(f)[1].lower() == '.xml' or os.path.splitext(f)[1].lower() == '.paper':
             shutil.copy(os.path.join(original_dir,f),cleaned_dir)
             continue
-        if os.path.splitext(f)[1].lower()=='.htm' or os.path.splitext(f)[1].lower()=='.txt':
-            count=count+1
+        if os.path.splitext(f)[1].lower() == '.htm' or os.path.splitext(f)[1].lower() == '.txt':
+            count = count + 1
             print f
-            print str(count)+ ' of '+str(len(original_folders))+' files\n'
+            print str(count) + ' of ' + str(len(original_folders)) + ' files\n'
             clean_htm_file(original_dir,cleaned_dir,f)
         else:
-            print 'Processing '+f+' ...\n'
+            print 'Processing ' + f + ' ...\n'
 
             if not os.path.isdir(os.path.join(cleaned_dir,f)):
                 os.mkdir(os.path.join(cleaned_dir,f))
+                os.mkdir(os.path.join(os.path.join(cleaned_dir,f), 'Make_Line'))
+                os.mkdir(os.path.join(os.path.join(cleaned_dir,f), 'Manual_Clean'))
 
             traverse_folders(os.path.join(original_dir,f),os.path.join(cleaned_dir,f))
+
 
 def clean_htm_file(original_dir,cleaned_dir,file):
 
     global tag_pattern
     global messy_pattern
 
-    content=open(os.path.join(original_dir,file))
-    content_lines=content.readlines()
-    content.close()
-
-    content_size=os.path.getsize(os.path.join(original_dir,file))
-
-    cleaned_file=open(os.path.join(cleaned_dir,file),'w')
-
     table_buffer=[]
     tag_pattern = re.compile('<.*?>')
     messy_pattern = re.compile('&.*?;')
 
-    if float(len(content_lines))/float(content_size)<=0.001:
-        file_clean=True
-        temp_lines=[]
+    original_content = open(os.path.join(original_dir,file))
+    original_lines = original_content.readlines()
+    original_len = len(original_lines)
+    original_size = os.path.getsize(os.path.join(original_dir,file))
 
-        for c in content_lines:
+    # Make Line
+    if float(original_len)/float(original_size) <= 0.0008:
+        file_clean = True
+        temp_lines = []
+        cleaned_file=open(os.path.join(os.path.join(cleaned_dir,'Make_Line'),file.split('.')[0] + '.txt'),'w')
+
+        # Add '\n' after every </tr>
+        for c in original_lines:
 
             if not '</tr>' in c:
                 temp_lines.append(c)
                 continue
 
-            index_list=[]
+            index_list = []
 
-            k=c.lower().find('</tr>')
+            k = c.lower().find('</tr>')
 
-            while k>=0:
+            while k >= 0:
                 index_list.append(k)
-                k=c.lower().find('</tr>',k+1)
+                k = c.lower().find('</tr>',k + 1)
 
-            if len(index_list)>=2:
-                temp_lines.append(c[:index_list[0]]+'\n')
-                for i in range(len(index_list)-1):
-                    temp_lines.append(c[index_list[i]:index_list[i+1]]+'\n')
-                temp_lines.append(c[index_list[i+1]:]+'\n')
+            if len(index_list) >= 2:
+                temp_lines.append(c[:index_list[0]] + '\n')
+                for i in range(len(index_list) - 1):
+                    temp_lines.append(c[index_list[i]:index_list[i + 1]] + '\n')
+                temp_lines.append(c[index_list[i + 1]:] + '\n')
 
-        if float(len(temp_lines))/float(content_size)<0.0003:
-            file_clean=False
+        if float(len(temp_lines))/float(original_size) < 0.0003:
+            file_clean = False
 
-        if file_clean==True:
-            content_lines=temp_lines
+        if file_clean == True:
+            original_lines = temp_lines
         else:
             shutil.copy(os.path.join(original_dir,file),os.path.join(cleaned_dir,'Manual_Clean'))
 
-    for c in content_lines:
+        # Gather entire table
 
-        if len(table_buffer) != 0:
+        for c in original_lines:
 
-            if '</table>' in c.lower():
-                table_buffer.append(c)
-                table_check(table_buffer, cleaned_file)
-                table_buffer = []
-            else:
-                table_buffer.append(c)
-                continue
-        else:
-            if '<table' in c.lower():
+            if len(table_buffer) != 0:
+
                 if '</table>' in c.lower():
                     table_buffer.append(c)
                     table_check(table_buffer, cleaned_file)
@@ -130,14 +126,39 @@ def clean_htm_file(original_dir,cleaned_dir,file):
                     table_buffer.append(c)
                     continue
             else:
-                text_replace(c, cleaned_file)
+                if '<table' in c.lower():
+                    if '</table>' in c.lower():
+                        table_buffer.append(c)
+                        table_check(table_buffer, cleaned_file)
+                        table_buffer = []
+                    else:
+                        table_buffer.append(c)
+                        continue
+                else:
+                    text_replace(c, cleaned_file)
 
-    if len(table_buffer)==0:
-        cleaned_file.close()
+        if len(table_buffer)==0:
+            cleaned_file.close()
+        else:
+            table_check(table_buffer, cleaned_file)
+            cleaned_file.close()
+
+
     else:
-        table_check(table_buffer, cleaned_file)
-        cleaned_file.close()
+        original_content = open(os.path.join(original_dir, file))
+        untagged_content = BeautifulSoup(original_content,'html.parser').get_text().encode('utf8')
+        original_content.close()
 
+        untagged_file = open(os.path.join(cleaned_dir,file.split('.')[0] + '.txt'),'w')
+        untagged_content = untagged_content.split('\n')
+        for uc in untagged_content:
+            if '\xc3' or '\xbd' or '\xc2' or '\xa0' or '\xe2' or '\x80' or '\x99' or '\x9c' or '\x9d' or '\x94' or '\x97' or '\xa6' or '\xa2' or'\'' in uc:
+                uc = uc.replace('\xc3','').replace('\xbd','').replace('\xc2','').replace('\xa0','').replace('\xe2','').replace('\x80','').replace('\x99','').replace('\x9c','').replace('\x9d','').replace('\x94','').replace('\x97','').replace('\xa6','').replace('\xa2','').replace("\'",r"'")
+                if uc != '' and not uc.isspace():
+                    untagged_file.write(uc + '\n')
+        untagged_file.close()
+
+# Clean entire table
 def table_check(table_buffer,cleaned_file):
 
     buffer_len=0
@@ -178,6 +199,7 @@ def table_check(table_buffer,cleaned_file):
                     cleaned_file.write(tb)
                 cleaned_file.write(tb)
 
+# Clean rest content
 def text_replace(text,cleaned_file):
 
     tag_set = tag_pattern.findall(text)
@@ -198,6 +220,7 @@ def text_replace(text,cleaned_file):
             cleaned_file.write(text)
 
         cleaned_file.write(text)
+
 
 
 if __name__=='__main__':
